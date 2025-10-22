c6c11cc7550a3ce4d20aaa92e73e6dd24e7e9635
Jaime <christianjaimesm@gmail.com>
2025-10-22 10:35:13 -0300

    Objetos y Callbacks

--- C8 Objects + Callbacks/objects/ejercicios/06.js
+++ C8 Objects + Callbacks/objects/ejercicios/06.js
@@ -2,6 +2,8 @@ const contarPropiedades = (objeto) => {
   // Cuenta y retorna el total de propiedades que tiene el objeto.
   // PISTA: Puedes iterarlo usando el bucle for-in.
   // Tu código:
+  const cantidadPropiedades = Object.keys(objeto).length;
+  return cantidadPropiedades;
 };
 
 module.exports = contarPropiedades;
--- C8 Objects + Callbacks/objects/ejercicios/12.js
+++ C8 Objects + Callbacks/objects/ejercicios/12.js
@@ -2,6 +2,8 @@ function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
   // El parámetro "objetoMisterioso" posee una propiedad con el nombre "numeroMisterioso".
   // Debes multiplicar este número por 5 y retornar el resultado.
   // Tu código:
+  const multiplicarNumero = objetoMisterioso.numeroMisterioso * 5;
+  return multiplicarNumero;
 }
 
 module.exports = multiplicarNumeroDesconocidoPorCinco;
